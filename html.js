f279b5a48a3058ead46590dccd74eb99487c5df8
landakdu <durilandak429@gmail.com>
2024-09-08 17:37:28 +0800

    Update html.js

--- html.js
+++ html.js
@@ -5,7 +5,7 @@ const menu1 = `
 
 <!-- Top Navigation Menu -->
 <div class="topnav">
-  <a href="https://qqcukurukukcokburrrrrr.on.drv.tw/rtpslot/" class="active tmenu"><img class="gmenu" src="icon/home.png"></a>
+  <a href="https://qqcukurukukcokburrrrrr.on.drv.tw/rtpslot1/" class="active tmenu"><img class="gmenu" src="icon/home.png"></a>
   <div id="myLinks">
     <a href="pragmatic.html" class="stay tmenu"><img class="gmenu" src="icon/pragmatic.png"></a>
     <a href="pgsoft.html" class="stay tmenu"><img class="gmenu" src="icon/pgsoft.png"></a>
