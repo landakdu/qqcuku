dd3d9f634d3f110ad6932f0cc5c4893207a3b0bf
landakdu <durilandak429@gmail.com>
2024-09-15 16:11:55 +0800

    Add files via upload

---
+++ htmlgdrive.js
@@ -0,0 +1,44 @@
+// code standing inside html.js for easy edit
+const menu1 = `
+<!-- Simulate a smartphone / tablet look -->
+<div class="mobile-container">
+
+<!-- Top Navigation Menu -->
+<div class="topnav">
+  <a href="../index.html" class="active tmenu"><img class="gmenu" src="../icon/home.png"></a>
+  <div id="myLinks">
+    <a href="../pragmatic/index.html" class="stay tmenu"><img class="gmenu" src="../icon/pragmatic.png"></a>
+    <a href="../pgsoft/index.html" class="stay tmenu"><img class="gmenu" src="../icon/pgsoft.png"></a>
+    <a href="../spadegaming/index.html" class="stay tmenu"><img class="gmenu" src="../icon/spadegaming.png"></a>
+    <a href="../fastspin/index.html" class="stay tmenu"><img class="gmenu" src="../icon/fastspin.png"></a>
+    <a href="../joker/index.html" class="stay tmenu"><img class="gmenu" src="../icon/joker.png"></a>
+    <a href="../boongo/index.html" class="stay tmenu"><img class="gmenu" src="../icon/boongo.png"></a>
+    <a href="../jili/index.html" class="stay tmenu"><img class="gmenu" src="../icon/jili.png"></a>
+    <a href="../playtech/index.html" class="stay tmenu"><img class="gmenu" src="../icon/playtech.png"></a>
+    <a href="../microgaming/index.html" class="stay tmenu"><img class="gmenu" src="../icon/microgaming.png"></a>
+    <a href="../habanero/index.html" class="stay tmenu"><img class="gmenu" src="../icon/habanero.png"></a>
+    <a href="../cq9/index.html" class="stay tmenu"><img class="gmenu" src="../icon/cq9.png"></a>
+    <a href="../playstar/index.html" class="stay tmenu"><img class="gmenu" src="../icon/playstar.png"></a>
+    <a href="../yggdrasil/index.html" class="stay tmenu"><img class="gmenu" src="../icon/yggdrasil.png"></a>
+    <a href="../netent/index.html" class="stay tmenu"><img class="gmenu" src="../icon/netent.png"></a>
+    <a href="../skywind/index.html" class="stay tmenu"><img class="gmenu" src="../icon/skywind.png"></a>
+    <a href="../toptrendgaming/index.html" class="stay tmenu"><img class="gmenu" src="../icon/toptrendgaming.png"></a>
+    <a href="../nextspin/index.html" class="stay tmenu"><img class="gmenu" src="../icon/nextspin.png"></a>
+    <a href="../bbin/index.html" class="stay tmenu"><img class="gmenu" src="../icon/bbin.png"></a>
+    <a href="../creativegaming/index.html" class="stay tmenu"><img class="gmenu" src="../icon/creativegaming.png"></a>
+    <a href="../evoplay/index.html" class="stay tmenu"><img class="gmenu" src="../icon/evoplay.png"></a>
+    <a href="../royalgaming/index.html" class="stay tmenu"><img class="gmenu" src="../icon/royalgaming.png"></a>
+    <a href="../fishing/index.html" class="stay tmenu"><img class="gmenu" src="../icon/fishing.png"></a>
+    <a href="../arcade/index.html" class="stay tmenu"><img class="gmenu" src="../icon/arcade.png"></a>
+    <a href="../casino/index.html" class="stay tmenu"><img class="gmenu" src="../icon/casino.png"></a>
+  </div>
+  <a href="javascript:void(0);" class="icon tmenu" onclick="myFunction()"><img class="gmenu" src="../icon/game.png"></a>
+</div>
+`;
+
+const menu2 = `
+<input id="searchbar" 
+               onkeyup="search_animal()" 
+               type="text" name="search" 
+               placeholder="cari nama permainan...">
+`;
