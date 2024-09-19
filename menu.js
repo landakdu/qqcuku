a8f8b0fc9bd85c93b85b028e8b86c9b50d1cf320
landakdu <durilandak429@gmail.com>
2024-09-19 17:51:35 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,5 +1,3 @@
-document.title = (location.pathname.substring(location.pathname.lastIndexOf("/") + 1)).replace('.html','');
-
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
